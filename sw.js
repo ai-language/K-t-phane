f73bff5b2f7c2a6e0edf9271b0e226d06c1a26e6
ai-language <toptaallanguage@gmail.com>
2026-05-31 01:46:53 +0200

    Update (navigator.serviceWorker.ready) sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('fetch',function(e){
   if(new URL(e.request.url).pathname==='/K-t-phane/player'){
     e.respondWith(
       caches.open('nhg-player').then(function(c){
-        return c.match(e.request).then(function(r){
+        return c.match('/K-t-phane/player').then(function(r){
           return r||new Response('<h1>Yükleniyor...</h1>',{status:200,headers:{'Content-Type':'text/html'}});
         });
       })
